c497f16ef1cc5f7ef03d0e3c82b97576e5695303
Siddarth <siddarth2801@gmail.com>
2023-06-22 02:34:04 +0530

    pagination server done

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -45,11 +45,21 @@ const getAllEvents = async (req, res) => {
     if (sort === 'z-a') {
         result = result.sort('-title');
     }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit);
     const events = await result;
+
+    const totalEvents = await Event.countDocuments(query);
+    const numOfPages = Math.ceil(totalEvents / limit);
+
     res.status(StatusCodes.OK).json({
         events,
-        totalEvents: events.length,
-        numOfPages: 1,
+        totalEvents,
+        numOfPages,
     });
 };
 
